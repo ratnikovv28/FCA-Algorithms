3198ade17089de50ffb8f968ffec6893cb019c37
Владимир <vladimir-ratnikov2002@mail.ru>
2023-03-18 12:39:34 +0300

    Add project files.

---
+++ FCA Algorithms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FCA_Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
