5fd85b84783f6311928b8cc4f19bbb79fafa2dbc
Владимир <vladimir-ratnikov2002@mail.ru>
2023-03-18 13:21:39 +0300

    Changes

--- FCA Algorithms/Program.cs
+++ FCA Algorithms/Program.cs
@@ -1,10 +1,42 @@
 ﻿namespace FCA_Algorithms
 {
+    class FormalContext
+    {
+        
+    }
+
+    class Concept
+    {
+        public string extent;
+        public string intent;
+    }
+    
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        public void AddIntent()
+        {
+
+        }
+
+        public void GetHighestNode()
+        {
+            bool parentIsHighest = true;
+
+            while (parentIsHighest)
+            {
+                parentIsHighest = false;
+
+            }
+        }
+
+        public void Add()
+        {
+
         }
     }
 }
