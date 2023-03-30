3c9783c03f523885c9e28c614b15c494c031f4d4
Владимир <vladimir-ratnikov2002@mail.ru>
2023-03-30 20:35:17 +0300

    Changes

--- FCA Algorithms/Program.cs
+++ FCA Algorithms/Program.cs
@@ -1,14 +1,50 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 
 namespace FCA_Algorithms
 {
+
+    public class Params
+    {
+        public List<string> AttrNames { get; set; }
+    }
+
+    public class ObjNames
+    {
+    }
+
+    public class RootObject
+    {
+        public ObjNames ObjNames { get; set; }
+        public Params Params { get; set; }
+    }
+
+    public class Data
+    {
+        public List<int> Inds { get; set; }
+    }
+
+    public class DataObject
+    {
+        public List<Data> Data { get; set; }
+    }
+
+    public class CombinedObject
+    {
+        public ObjNames ObjNames { get; set; }
+        public Params Params { get; set; }
+        public List<Data> Data { get; set; }
+    }
+
     class FormalContext
     {
         private List<string> _g;
         private List<string> _m;
-        private Dictionary<string, HashSet<string>> _i;
+        private Dictionary<string, List<string>> _i;
 
         public List<string> M
         {
@@ -22,13 +58,13 @@ namespace FCA_Algorithms
             set { _g = value; }
         }
 
-        public Dictionary<string, HashSet<string>> I
+        public Dictionary<string, List<string>> I
         {
             get { return _i; }
             set { _i = value; }
         }
 
-        public FormalContext(List<string> g, List<string> m, Dictionary<string, HashSet<string>> i)
+        public FormalContext(List<string> g, List<string> m, Dictionary<string, List<string>> i)
         {
             G = g;
             M = m;
@@ -54,9 +90,9 @@ namespace FCA_Algorithms
             return false;
         }
 
-        public HashSet<string> GetObjectsWithAttribute(string attribute)
+        public List<string> GetObjectsWithAttribute(string attribute)
         {
-            HashSet<string> objects = new HashSet<string>();
+            List<string> objects = new List<string>();
             foreach (var obj in G)
             {
                 if (HasIncidence(obj, attribute))
@@ -67,34 +103,34 @@ namespace FCA_Algorithms
             return objects;
         }
 
-        public HashSet<string> GetAttributesOfObject(string obj)
+        public List<string> GetAttributesOfObject(string obj)
         {
             if (I.ContainsKey(obj))
             {
                 return I[obj];
             }
-            return new HashSet<string>();
+            return new List<string>();
         }
     }
 
     public class Concept
     {
-        private string _extent;
-        private string _intent;
+        private List<string> _extent;
+        private List<string> _intent;
 
-        public string Extent
+        public List<string> Extent
         {
             get { return _extent; }
             set { _extent = value; }
         }
 
-        public string Intent
+        public List<string> Intent
         {
             get { return _intent; }
             set { _intent = value; }
         }
 
-        public Concept(string extent, string intent)
+        public Concept(List<string> extent, List<string> intent)
         {
             _extent = extent;
             _intent = intent;
@@ -105,21 +141,46 @@ namespace FCA_Algorithms
     {
         static void Main(string[] args)
         {
-            // Создаем формальный контекст
-            List<string> g = new List<string>() { "1", "2", "3" };
-            List<string> m = new List<string>() { "a", "b", "c", "d" };
-            Dictionary<string, HashSet<string>> i = new Dictionary<string, HashSet<string>>();
-            i.Add("1", new HashSet<string>() { "a", "c" });
-            i.Add("2", new HashSet<string>() { "b", "c" });
-            i.Add("3", new HashSet<string>() { "a", "b", "c", "d" });
-            FormalContext fc = new FormalContext(g, m, i);
+            //// Создаем формальный контекст
+            //List<string> g = new List<string>() { "1", "2", "3" };
+            //List<string> m = new List<string>() { "a", "b", "c", "d" };
+            //Dictionary<string, List<string>> i = new Dictionary<string, List<string>>();
+            //i.Add("1", new List<string>() { "a", "c" });
+            //i.Add("2", new List<string>() { "b", "c" });
+            //i.Add("3", new List<string>() { "a", "b", "c", "d" });
+            //FormalContext fc = new FormalContext(g, m, i);
 
-            Dictionary<Concept, List<Concept>> lattice = AddIntent(fc);
+            //Dictionary<Concept, List<Concept>> lattice = AddIntent(fc);
+
+            string fileName = "data.json";
+            string jsonString = File.ReadAllText(fileName);
+
+            var combinedObjects = JsonConvert.DeserializeObject<List<CombinedObject>>(jsonString);
+
+            //Создаем формальный контекст на основании входного файла
+            var objectWithIntents = combinedObjects[1].Data;
+
+            var G = new List<string>();
+            var M = combinedObjects[0].Params.AttrNames;
+            var I = new Dictionary<string, List<string>>();
+
+            //Добавляем в матрицу инцидентности формальные понятия
+            for (int i = 0; i < objectWithIntents.Count(); i++)
+            {
+                G.Add((i + 1).ToString());
+                I.Add((i + 1).ToString(), objectWithIntents[i].Inds.Select(intent => M[intent]).ToList());
+            }
+
+            var fc = new FormalContext(G, M, I);
+
+            var addIntent = AddIntent(fc);
+
+            Console.WriteLine();
         }
 
         public static Dictionary<Concept, List<Concept>> AddIntent(FormalContext fc)
         {
-            Concept bottomConcept = new Concept("", String.Join("", fc.M.ToArray()));
+            Concept bottomConcept = new Concept(new List<string>(), fc.M);
 
             Dictionary<Concept, List<Concept>> lattice = new Dictionary<Concept, List<Concept>>()
             {
@@ -128,7 +189,7 @@ namespace FCA_Algorithms
 
             foreach (var g in fc.G)
             {
-                Concept objectConcept = Add(String.Join("", fc.GetAttributesOfObject(g)), bottomConcept, lattice);
+                Concept objectConcept = Add(fc.GetAttributesOfObject(g), bottomConcept, lattice);
 
                 AddGToExtentAbove(g, objectConcept, lattice);
             }
@@ -141,7 +202,7 @@ namespace FCA_Algorithms
         public static void AddGToExtentAbove(string g, Concept objectConcept, Dictionary<Concept, List<Concept>> lattice)
         {
             if(!objectConcept.Extent.Contains(g))
-                objectConcept.Extent += g;
+                objectConcept.Extent.Add(g);
 
             var parents = lattice[objectConcept];
 
@@ -154,7 +215,7 @@ namespace FCA_Algorithms
             lattice.Add(objectConcept, parents);
         }
 
-        public static Concept GetHighestNodeOfIntent(string intent, Concept generatorConcept, Dictionary<Concept, List<Concept>> lattice)
+        public static Concept GetHighestNodeOfIntent(List<string> intent, Concept generatorConcept, Dictionary<Concept, List<Concept>> lattice)
         {
             bool parentIsHighest = true;
 
@@ -167,7 +228,7 @@ namespace FCA_Algorithms
                 {
                     foreach (var parent in parents)
                     {
-                        if (parent.Intent.Contains(intent))
+                        if (intent.All(item => parent.Intent.Contains(item)))
                         {
                             generatorConcept = parent;
                             parentIsHighest = true;
@@ -180,31 +241,31 @@ namespace FCA_Algorithms
             return generatorConcept;
         }
 
-        public static Concept Add(string intent, Concept generatorConcept, Dictionary<Concept, List<Concept>> lattice)
+        public static Concept Add(List<string> intent, Concept generatorConcept, Dictionary<Concept, List<Concept>> lattice)
         {
             generatorConcept = GetHighestNodeOfIntent(intent, generatorConcept, lattice);
             if (generatorConcept.Intent == intent) return generatorConcept;
 
-            List<Concept> generatorParents = lattice[generatorConcept].ToList();
+            List<Concept> generatorParents = lattice[generatorConcept];
             List<Concept> newParents = new List<Concept>();
 
             if (generatorParents != null)
             {
                 for (int i = 0; i < generatorParents.Count; i++)
                 {
-                    if (!intent.Contains(generatorParents[i].Intent))
-                        generatorParents[i] = Add(new string(generatorParents[i].Intent.Intersect(intent).ToArray()), generatorParents[i], lattice);
+                    if (!generatorParents[i].Intent.All(item => intent.Contains(item)))
+                        generatorParents[i] = Add(generatorParents[i].Intent.Intersect(intent).ToList(), generatorParents[i], lattice);
 
                     bool addParent = true;
 
                     foreach (var parent in newParents)
                     {
-                        if (intent.Contains(generatorParents[i].Intent))
+                        if (generatorParents[i].Intent.All(item => intent.Contains(item)))
                         {
                             addParent = false;
                             break;
                         }
-                        else if (generatorParents[i].Intent.Contains(parent.Intent)) 
+                        else if (parent.Intent.All(item => generatorParents[i].Intent.Contains(item))) 
                             newParents.Remove(parent);
                     }
 
